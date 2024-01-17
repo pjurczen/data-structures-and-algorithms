4d3d55c5b8be19efbbcc5161448046bb5031ee51
Piotr Jurczenko <16348752+pjurczen@users.noreply.github.com>
2024-01-17 19:49:18 +0100

    BinaryTree

--- src/main/java/com/pjurczen/BinaryTree.java
+++ src/main/java/com/pjurczen/BinaryTree.java
@@ -10,6 +10,22 @@ public class BinaryTree {
         this.root = addRecursive(root, value);
     }
 
+    public int search(Integer value) {
+        return searchRecursive(root, value);
+    }
+
+    private int searchRecursive(Node<Integer> current, Integer value) {
+        if (current == null) {
+            return 0;
+        } else if (Objects.equals(current.value, value)) {
+            return current.count;
+        } else if (value < current.value) {
+            return searchRecursive(current.left, value);
+        } else { // value > rootNode.value
+            return searchRecursive(current.right, value);
+        }
+    }
+
     private Node<Integer> addRecursive(Node<Integer> current, Integer value) {
         if (current == null) {
             return new Node<>(value);
@@ -17,9 +33,11 @@ public class BinaryTree {
             current.count++;
             return current;
         } else if (value < current.value) {
-            return addRecursive(current.left, value);
+            current.left = addRecursive(current.left, value);
+            return current;
         } else { // value > rootNode.value
-            return addRecursive(current.right, value);
+            current.right = addRecursive(current.right, value);
+            return current;
         }
     }
 
---
+++ src/test/java/com/pjurczen/BinaryTreeTest.java
@@ -0,0 +1,25 @@
+package com.pjurczen;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BinaryTreeTest {
+
+    @Test
+    void addAndSearch() {
+        final var binaryTree = new BinaryTree();
+        binaryTree.add(5);
+        binaryTree.add(2);
+        binaryTree.add(8);
+        binaryTree.add(10);
+        binaryTree.add(7);
+        binaryTree.add(1);
+        binaryTree.add(2);
+        binaryTree.add(-1);
+        binaryTree.add(7);
+        binaryTree.add(2);
+        final var result = binaryTree.search(2);
+        assertThat(result).isEqualTo(3);
+    }
+}
