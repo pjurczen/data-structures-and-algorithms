7c2a922cebd12b2f25df101ff760bfc14803ce90
Piotr Jurczenko <16348752+pjurczen@users.noreply.github.com>
2024-01-18 21:30:10 +0100

    MergeSort

--- src/main/java/com/pjurczen/MergeSort.java
+++ src/main/java/com/pjurczen/MergeSort.java
@@ -17,23 +17,22 @@ public class MergeSort {
         int[] left = new int[n1];
         int[] right = new int[n2];
         for (int i=0; i < n1; i++) {
-            left[i] = input[i+l];
+            left[i] = input[l+i];
         }
         for (int i=0; i < n2; i++) {
-            right[i] = input[i+l+n1];
+            right[i] = input[pivot+1+i];
         }
-        int i = 0, j = 0;
+        int i = 0,j = 0;
         int k = l;
         while (i < n1 && j < n2) {
             if (left[i] <= right[j]) {
                 input[k] = left[i];
                 i++;
-                k++;
             } else {
                 input[k] = right[j];
                 j++;
-                k++;
             }
+            k++;
         }
         while (i < n1) {
             input[k] = left[i];
