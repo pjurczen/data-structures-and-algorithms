4f768cf75e6e6605aab3ac6b63dc6126f2aec578
Piotr Jurczenko <16348752+pjurczen@users.noreply.github.com>
2024-01-26 17:48:36 +0100

    MinimumSizeSubarraySum with binary search and O(N logN)

--- src/main/java/com/pjurczen/MinimumSizeSubarraySum.java
+++ src/main/java/com/pjurczen/MinimumSizeSubarraySum.java
@@ -2,12 +2,16 @@ package com.pjurczen;
 
 public class MinimumSizeSubarraySum {
     public int minSubArrayLen(int target, int[] nums) {
+        return solveNLogN(target, nums);
+    }
+
+    private int solveN(int target, int[] nums) {
         int bestResult = Integer.MAX_VALUE;
         int left = 0;
         int right = 0;
         int sum = 0;
         int count = 0;
-        for ( ;right < nums.length; right++) {
+        for (; right < nums.length; right++) {
             sum += nums[right];
             count++;
             if (sum >= target) {
@@ -26,5 +30,37 @@ public class MinimumSizeSubarraySum {
             return 0;
         }
         return bestResult;
+    }
+
+    private int solveNLogN(int s, int[] nums) {
+        var sums = new int[nums.length + 1];
+        sums[0] = 0;
+        int bestCount = Integer.MAX_VALUE;
+        for (int i=1; i <= nums.length; i++) {
+            sums[i] = sums[i-1] + nums[i-1];
+        }
+        for (int i=0; i < nums.length; i++) {
+            int endIndex = binarySearch(sums, i+1, nums.length, s + sums[i]);
+            if (endIndex == -1) {
+                break;
+            }
+            var count = endIndex - i;
+            if (count < bestCount) {
+                bestCount = count;
+            }
+        }
+        return bestCount == Integer.MAX_VALUE ? 0 : bestCount;
+    }
+
+    private int binarySearch(int[] sums, int start, int end, int target) {
+        if (end - start < 1) {
+            return sums[start] >= target ? start : -1;
+        }
+        var pivot = start + (end - start) / 2;
+        var left = binarySearch(sums, start, pivot, target);
+        if (left != -1) {
+            return left;
+        }
+        return binarySearch(sums, pivot + 1, end, target);
     }
 }
